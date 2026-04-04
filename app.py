c9c891a776fb5e67b57e09b63ee90ded83fd4f31
Jon-Beadle-Git <mj@hall747.com>
2026-04-04 13:49:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.title("Tell Ro and Grampy Your Name")
 st.write("Paste text here for diagnosis")
 
-fam_dict = {"Jon": "brilliant", "Will": "foolish wood-maker", "Cath": "kitchen perfectionista", "Em": "too many outbuildings"}
 
 text = st.text_area ("Your text", height =200)
 
 if text:
       
+	fam_dict = {"Jon": "brilliant", "Will": "foolish wood-maker", "Cath": "kitchen perfectionista", "Em": "too many outbuildings"}
 	    
 	st.subheader("How Much Do You Love Ro and Grampy")
 	st.write(f"**{text}** loves Ro and Grampy very much")
