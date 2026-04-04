a942bad47f360d5c27b30abd66b3d77d3fa92181
Jon-Beadle-Git <mj@hall747.com>
2026-04-04 13:42:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ if text:
 	st.subheader("How Much Do You Love Ro and Grampy")
 	st.write(f"**{text}** loves Ro and Grampy very much")
 	new_text = ""
-	for letter in text:
-		new_text += letter.upper()
-	print (f"""Here's your name in UPPER CASE: {new_text}""")
+	
+	#for letter in text:
+	#	new_text += letter.upper()
+	#print (f"""Here's your name in UPPER CASE: {new_text}""")
 
-print (fam_dict[text])
+	print (fam_dict[text])
 
 
 	
