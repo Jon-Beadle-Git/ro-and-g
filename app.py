b86c1abc08f71bdcb344eca98fcbc55441d3ab98
Jon-Beadle-Git <mj@hall747.com>
2026-04-07 19:39:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ text = st.text_area ("Your text", height =200)
 
 if text:
       
-	fam_dict = {"Jon": "brilliant", "Will": "foolish wood-maker", "Cath": "kitchen perfectionista", "Em": "too many outbuildings"}
+	fam_dict = {"Jon": "brilliant", "Will": "foolish wood-maker", "Cath": "kitchen perfectionista", "Em": "too many outbuildings", "Dave": "only lifts 5kg dumbbells"}
 	    
 	st.subheader("How Much Do You Love Ro and Grampy")
 	st.write(f"**{text}** loves Ro and Grampy very much")
